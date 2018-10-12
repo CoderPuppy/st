d80bff7c0011730a9321e059424f6b140c0d9fa2
CoderPuppy <coderpup@gmail.com>
2018-10-12 18:03:54 -0400

    update

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Source Code Pro:pixelsize=11:antialias=true:autohint=false";
+static char font[] = "Source Code Pro:pixelsize=10:antialias=true:autohint=false";
 static int borderpx = 2;
 static char shell[] = "/bin/sh";
 static char *utmp = NULL;
